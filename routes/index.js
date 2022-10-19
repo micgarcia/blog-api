36f54eea9e698ab6845b404bca85258c4aec60bd
micgarcia <micgarcia9185@gmail.com>
2022-10-19 14:28:18 -0700

    started setting up routes

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,22 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.json({'home': 'home page'});
 });
+
+// GET list of posts
+router.get('/posts', function(req, res, next) {
+  res.json({'posts': 'list of posts'});
+})
+
+// GET specific post
+router.get('/posts/:id', function(req, res, next) {
+  res.json({'post': `post number ${req.params.id}`});
+})
+
+// GET specific post's comments
+router.get('/posts/:id/comments', function(req, res, next) {
+  res.json({'comments': 'list of comments for post ' + req.params.id});
+})
 
 module.exports = router;
